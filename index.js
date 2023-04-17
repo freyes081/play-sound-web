ea547e8f5a2f160ad5f1bbe9e2ef33f743636270
Franco <freyes081@gmail.com>
2023-04-17 19:02:03 -0400

    Job done

--- index.js
+++ index.js
@@ -4,47 +4,18 @@ for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', function () {
 
         const buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case 'w':
-                const tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();                
-                break;
-            case 'a':
-                const tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play();
-                break;
-            case 's':
-                const tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play();
-                break;
-            case 'd':
-                const tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play();
-                break;
-            case 'j':
-                const snare = new Audio('sounds/snare.mp3');
-                snare.play();
-                break;
-            case 'k':
-                const crash = new Audio('sounds/crash.mp3');
-                crash.play();
-                break;
-            case 'l':
-                const kick = new Audio('sounds/kick-bass.mp3');
-                kick.play();
-                break;
-
-
-        
-            default:console.log(buttonInnerHTML);
-                break;
-        }
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener('keydown', function (event) {
-    const buttonInnerHTML = event.key;
-    switch (buttonInnerHTML) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
         case 'w':
             const tom1 = new Audio('sounds/tom-1.mp3');
             tom1.play();
@@ -73,10 +44,23 @@ document.addEventListener('keydown', function (event) {
             const kick = new Audio('sounds/kick-bass.mp3');
             kick.play();
             break;
+        
+        default:console.log(buttonInnerHTML); 
+    
+}}
 
-        default:console.log(buttonInnerHTML);    
-}
-});
+
+function buttonAnimation (key) {
+    const activeButton = document.querySelector("." + key);
+    activeButton.classList.add('pressed');
+    setTimeout(() => {
+        activeButton.classList.remove('pressed');
+      }, 100);
+      
+
+    }
+    
+
 
 
 
