26085f4a5f17d4d1d8bcf0282ae2ca66b0f0c749
Frank De los Reyes <freyes@bcoa47.cu>
2023-04-17 15:20:26 -0400

    add keypress listener for testing

--- index.js
+++ index.js
@@ -42,6 +42,43 @@ for (let i = 0; i < button.length; i++) {
     });
 }
 
+document.addEventListener('keydown', function (event) {
+    const buttonInnerHTML = event.key;
+    switch (buttonInnerHTML) {
+        case 'w':
+            const tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            const tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            const tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            const tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            const snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'k':
+            const crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'l':
+            const kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        default:console.log(buttonInnerHTML);    
+}
+});
+
+
 
 
 
